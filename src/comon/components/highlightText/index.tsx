30ea2bc948c5dbcef73eea1b0845c01fcc8d843a
maksym-debutify <maksym.humeniuk@debutify.com>
2024-07-03 00:26:26 +0300

    core(fixed last deploy)

--- src/comon/components/highlightText/index.tsx
+++ src/comon/components/highlightText/index.tsx
@@ -2,13 +2,17 @@ import { Fragment, ReactNode } from 'react';
 import { PinkText } from '@/components/email/styles';
 
 export const highlight = (text: string, terms: string[]): ReactNode => {
-  const regex = new RegExp(`(${terms.join('|')})`, 'gi');
-  const parts = text.split(regex);
+  try {
+    const regex = new RegExp(`(${terms.join('|')})`, 'gi');
+    const parts = text.split(regex);
 
-  return parts.map((part, index) => {
-    if (terms.includes(part)) {
-      return <PinkText key={index}>{part}</PinkText>;
-    }
-    return <Fragment key={index}>{part}</Fragment>;
-  });
+    return parts.map((part, index) => {
+      if (terms.includes(part)) {
+        return <PinkText key={index}>{part}</PinkText>;
+      }
+      return <Fragment key={index}>{part}</Fragment>;
+    });
+  } catch (e) {
+    localStorage.clear();
+  }
 };
